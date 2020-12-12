3e417c1134a23b039947fe466b23ab8b97704cea
Luokun <olooook@outlook.com>
2020-12-12 19:23:51 +0800

    .

--- tools/progress.py
+++ tools/progress.py
@@ -15,7 +15,7 @@ class ProgressBar:
         t_delta = timedelta(seconds=int(time() - self.t_start))
         circles = '●' * (self.n_circles * self.phase // self.total)
         circles = circles.ljust(self.n_circles, '○')
-        line = f'\r[{circles}] [{self.phase}/{self.total} {t_delta}] {postfix}'
+        line = f'\r\033[32m[{circles}]\033[0m [{self.phase}/{self.total} {t_delta}] {postfix}'
         if self.line_len > len(line):
             line = line.ljust(self.line_len)
         self.line_len = len(line)
@@ -30,3 +30,11 @@ class ProgressBar:
 
     def __exit__(self, *args, **kwargs):
         self.close()
+
+
+if __name__ == "__main__":
+    from time import sleep
+    with ProgressBar(total=100) as bar:
+        for i in range(100):
+            bar.update()
+            sleep(.1)
