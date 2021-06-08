b8cebbf0ac49e66cd2e1e093f1bbce60b1ed4217
Luokun <olooook@outlook.com>
2021-06-08 09:52:11 +0800

    set the default value of epochs to 200

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ parser.add_argument('--checkpoint', type=str, default=None, help='Checkpoint nam
 
 parser.add_argument('--batch', type=int, default=64, help='Batch size')
 parser.add_argument('--lr', type=float, default=1e-3, help='Learn rate')
-parser.add_argument('--epochs', type=int, default=100, help='Number of epochs')
+parser.add_argument('--epochs', type=int, default=200, help='Number of epochs')
 parser.add_argument('--workers', type=int, default=0, help='Number of data loader workers')
 parser.add_argument('--gpu', type=int, default=None, help='GPU')
 parser.add_argument('--gpus', type=str, default=None, help='GPUs')
