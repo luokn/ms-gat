5f2b026a9edd2da33c54ddcc5b7290b2cdcf1c94
Kun Luo <olooook@outlook.com>
2021-07-04 21:23:28 +0800

    .

--- lib/metrics.py
+++ lib/metrics.py
@@ -17,20 +17,19 @@ class Metrics:
     def __init__(self, mask_value=.0):
         self.n, self.mask_value = 0, mask_value
         self.AE, self.APE, self.SE = .0, .0, .0
-        self.MAE, self.MAPE, self.RMSE = .0, .0, .0
 
     def __call__(self, output: torch.Tensor, target: torch.Tensor):
         self.n += target.nelement()
         # MAE
         self.AE += torch.abs(output - target).sum().item()
-        self.MAE = self.AE / self.n
+        MAE = self.AE / self.n
         # MAPE
         mask = target > self.mask_value
         masked_output, masked_target = output[mask], target[mask]
         self.APE += 100 * torch.abs((masked_output - masked_target) / masked_target).sum().item()
-        self.MAPE = self.APE / self.n
+        MAPE = self.APE / self.n
         # RMSE
         self.SE += torch.square(output - target).sum().item()
-        self.RMSE = (self.SE / self.n)**.5
+        RMSE = (self.SE / self.n)**.5
         # stats
-        return {'MAE': self.MAE, 'MAPE': self.MAPE, 'RMSE': self.RMSE}
+        return {'MAE': MAE, 'MAPE': MAPE, 'RMSE': RMSE}
