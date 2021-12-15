ee11579d79474a6f9c0047b60d67e122eb0601e9
Luokun <olooook@outlook.com>
2021-12-15 16:58:06 +0800

    .

--- README.md
+++ README.md
@@ -9,19 +9,19 @@
 
 1.  _Train_
 
-    -   single GPU:
+    -   Single GPU:
 
         ```bash
         # PEMSD4
-        python3 ./main.py train -a data/pemsd4/pemsd4.csv -d data/pemsd4/pemsd4.npz \
+        python3 ./train.py -a data/pemsd4/pemsd4.csv -d data/pemsd4/pemsd4.npz \
             -o checkpoints/pemsd4/debug -n 307 -c 3 -b 64 -j 4
         ```
 
-    -   multiple GPUs:
+    -   Multiple GPUs:
 
         ```bash
         # PEMSD4
-        python3 ./main.py train -a data/pemsd4/pemsd4.csv -d data/pemsd4/pemsd4.npz \
+        python3 ./train.py -a data/pemsd4/pemsd4.csv -d data/pemsd4/pemsd4.npz \
             -o checkpoints/pemsd4/debug -n 307 -c 3 -b 64 -j 4 --gpus 0,1,2,3
         ```
 
@@ -29,14 +29,14 @@
 
     ```bash
     # PEMSD4
-    python3 ./main.py eval -a data/pemsd4/pemsd4.csv -d data/pemsd4/pemsd4.npz \
+    python3 ./eval.py -a data/pemsd4/pemsd4.csv -d data/pemsd4/pemsd4.npz \
         -o checkpoints/pemsd4/debug -n 307 -c 3 -b 64 -j 4 \
         -f checkpoints/pemsd4/debug/xx_loss\=xxx.xx.pkl
     ```
 
 **_Checkpoints_**
 
--   _PEMSD3, 2080TI-11G:_ [<img src="https://img.shields.io/badge/PEMSD3-MAE=15.60_MAPE=16.36_RMSE=26.36-4EAA25?style=flat-square"/>](https://drive.google.com/file/d/16bUCaI4p23vTGdMOXRRT45TNqci7VLCi/view?usp=sharing)
--   _PEMSD4, 2080TI-11G:_ [<img src="https://img.shields.io/badge/PEMSD4-MAE=19.58_MAPE=13.52_RMSE=31.72-4EAA25?style=flat-square"/>](https://drive.google.com/file/d/1CzS1-OCZXP6g8jM_CAMZGWlHRDyRxDBH/view?usp=sharing)
--   _PEMSD7, 2080TI-11G:_ [<img src="https://img.shields.io/badge/PEMSD7-MAE=20.44_MAPE=8.85_RMSE=34.11-4EAA25?style=flat-square">](https://drive.google.com/file/d/1a9VdvFOaMGU9-JyeRlDUDlzjHdrsEKSr/view?usp=sharing)
--   _PEMSD8, 2080TI-11G:_ [<img src="https://img.shields.io/badge/PEMSD8-MAE=14.58_MAPE=10.10_RMSE=23.94-4EAA25?style=flat-square"/>](https://drive.google.com/file/d/18_mJtL0G6KQZF8QxSLQu9THFg-h_46q-/view?usp=sharing)
+-   _PEMSD3:_ [<img src="https://img.shields.io/badge/PEMSD3-MAE=15.60_MAPE=16.36_RMSE=26.36-4EAA25?style=flat-square"/>](https://drive.google.com/file/d/16bUCaI4p23vTGdMOXRRT45TNqci7VLCi/view?usp=sharing)
+-   _PEMSD4:_ [<img src="https://img.shields.io/badge/PEMSD4-MAE=19.58_MAPE=13.52_RMSE=31.72-4EAA25?style=flat-square"/>](https://drive.google.com/file/d/1CzS1-OCZXP6g8jM_CAMZGWlHRDyRxDBH/view?usp=sharing)
+-   _PEMSD7:_ [<img src="https://img.shields.io/badge/PEMSD7-MAE=20.44_MAPE=8.85_RMSE=34.11-4EAA25?style=flat-square">](https://drive.google.com/file/d/1a9VdvFOaMGU9-JyeRlDUDlzjHdrsEKSr/view?usp=sharing)
+-   _PEMSD8:_ [<img src="https://img.shields.io/badge/PEMSD8-MAE=14.58_MAPE=10.10_RMSE=23.94-4EAA25?style=flat-square"/>](https://drive.google.com/file/d/18_mJtL0G6KQZF8QxSLQu9THFg-h_46q-/view?usp=sharing)
---
+++ eval.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Author  : Kun Luo
+# @Email   : olooook@outlook.com
+# @File    : main.py
+# @Date    : 2021/06/02
+# @Time    : 17:05:00
+
+
+import click
+from torch.nn import DataParallel
+
+from data import load_adj, load_data
+from engine import Evaluator
+from models import msgat
+
+models = {"ms-gat": msgat.msgat72, "ms-gat48": msgat.msgat48, "ms-gat72": msgat.msgat72, "ms-gat96": msgat.msgat96}
+
+
+@click.command(help="Evaluate.")
+@click.option("-a", "--adj-file", type=str, help="Graph adjacency file.")
+@click.option("-d", "--data-file", type=str, help="Time series data file.")
+@click.option("-f", "--ckpt-file", type=str, help="Pre-saved checkpoint file.")
+@click.option("-o", "--out-dir", type=str, help="Output directory for logs and records.")
+@click.option("-n", "--num-nodes", type=int, help="The number of nodes in the graph.")
+@click.option("-c", "--num-channels", type=int, help="Number of time series data channels.")
+@click.option("-j", "--num-workers", type=int, help="Number of data loader workers.", default=0)
+@click.option("-i", "--in-hours", type=str, help="Sampling hours.", default="1,2,3,24,168")
+@click.option("-b", "--batch-size", type=int, help="Batch size.", default=64)
+@click.option("--model", type=click.Choice(models.keys()), help="Model.", default="ms-gat")
+@click.option("--gpus", type=str, help="GPUs.", default="0")
+@click.option("--delta", type=float, help="Huber loss delta.", default=60)
+@click.option("--te/--no-te", help="With/without time embedding.", default=True)
+@click.option("--out-timesteps", type=int, help="Number of output timesteps.", default=12)
+@click.option("--timesteps-per-hour", type=int, help="Timesteps per hour.", default=12)
+def eval(
+    adj_file,
+    data_file,
+    ckpt_file,
+    out_dir,
+    num_nodes,
+    num_channels,
+    num_workers,
+    in_hours,
+    batch_size,
+    model,
+    gpus,
+    delta,
+    te,
+    out_timesteps,
+    timesteps_per_hour,
+):
+    in_hours, gpus = [int(i) for i in in_hours.split(",")], [int(i) for i in gpus.split(",")]
+    # data loaders
+    data_loaders = load_data(
+        data_file=data_file,
+        batch_size=batch_size,
+        in_hours=in_hours,
+        out_timesteps=out_timesteps,
+        timesteps_per_hour=timesteps_per_hour,
+        num_workers=num_workers,
+    )
+    # network
+    net = models[model](
+        n_components=len(in_hours),
+        in_channels=num_channels,
+        in_timesteps=timesteps_per_hour,
+        out_timesteps=out_timesteps,
+        adjacency=load_adj(adj_file, num_nodes),
+        use_te=te,
+        init_params=False,
+    )
+    # enable GPU.
+    if len(gpus) > 1:
+        net = DataParallel(net, device_ids=gpus)
+    net = net.cuda(gpus[0])
+    # eval
+    evaluator = Evaluator(model=net, ckpt_file=ckpt_file, out_dir=out_dir, delta=delta)
+    evaluator.eval(data_loaders[-1], gpu=gpus[0])
+
+
+if __name__ == "__main__":
+    eval()
