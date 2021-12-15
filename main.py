5ed816c3c56a5e2fb15c088d594c9c7af1897735
Luokun <olooook@outlook.com>
2021-12-15 16:19:41 +0800

    .

--- main.py
+++ main.py
@@ -6,10 +6,8 @@
 # @Date    : 2021/06/02
 # @Time    : 17:05:00
 
-import os
 
 import click
-from torch.functional import Tensor
 from torch.nn import DataParallel
 
 from data import load_adj, load_data
@@ -43,50 +41,65 @@ def cli():
 @click.option("--te/--no-te", help="With/without time embedding.", default=True)
 @click.option("--out-timesteps", type=int, help="Number of output timesteps.", default=12)
 @click.option("--timesteps-per-hour", type=int, help="Timesteps per hour.", default=12)
-def train(**args):
+def train(
+    adj_file,
+    data_file,
+    ckpt_file,
+    out_dir,
+    num_nodes,
+    num_channels,
+    num_workers,
+    in_hours,
+    batch_size,
+    epochs,
+    lr,
+    weight_decay,
+    model,
+    gpus,
+    delta,
+    te,
+    out_timesteps,
+    timesteps_per_hour,
+):
+    in_hours, gpus = [int(i) for i in in_hours.split(",")], [int(i) for i in gpus.split(",")]
     # data loaders
     data_loaders = load_data(
-        data_file=args["data_file"],
-        batch_size=args["batch_size"],
-        in_hours=[int(h) for h in args["in_hours"].split(",")],
-        out_timesteps=args["out_timesteps"],
-        timesteps_per_hour=args["timesteps_per_hour"],
-        num_workers=args["num_workers"],
+        data_file=data_file,
+        batch_size=batch_size,
+        in_hours=in_hours,
+        out_timesteps=out_timesteps,
+        timesteps_per_hour=timesteps_per_hour,
+        num_workers=num_workers,
     )
     # model
-    net = models[args["model"]](
-        n_components=len(args["in_hours"].split(",")),
-        in_channels=args["num_channels"],
-        in_timesteps=args["timesteps_per_hour"],
-        out_timesteps=args["out_timesteps"],
-        adjacency=load_adj(args["adj_file"], args["num_nodes"]),
-        use_te=args["te"],
-        init_params=args["ckpt_file"] is None,
+    net = models[model](
+        n_components=len(in_hours),
+        in_channels=num_channels,
+        in_timesteps=timesteps_per_hour,
+        out_timesteps=out_timesteps,
+        adjacency=load_adj(adj_file, num_nodes),
+        use_te=te,
+        init_params=ckpt_file is None,
     )
     # enable GPU.
-    if len(args["gpus"].split(",")) > 1:
-        gpu_id = None
-        net = DataParallel(net, device_ids=[int(i) for i in args["gpus"].split(",")]).cuda()
-    else:
-        gpu_id = int(args["gpus"])
-        net = net.cuda(gpu_id)
+    if len(gpus) > 1:
+        net = DataParallel(net, device_ids=gpus)
+    net = net.cuda(gpus[0])
     # train
     trainer = Trainer(
         model=net,
-        out_dir=args["out_dir"],
-        delta=args["delta"],
-        lr=args["lr"],
-        weight_decay=args["weight_decay"],
+        out_dir=out_dir,
+        delta=delta,
+        lr=lr,
+        weight_decay=weight_decay,
     )
-    if args["ckpt_file"] is not None:
-        trainer.load(ckpt_file=args["ckpt_file"])
-    trainer.fit((data_loaders[0], data_loaders[1]), epochs=args["epochs"], gpu=gpu_id)
+    if ckpt_file is not None:
+        trainer.load(ckpt_file=ckpt_file)
+    trainer.fit((data_loaders[0], data_loaders[1]), epochs=epochs, gpu=gpus[0])
     print("Training completed!")
     # eval
-    evaluator = Evaluator(
-        model=net, ckpt_file=trainer.out_dir / trainer.best["ckpt"], out_dir=args["out_dir"], delta=args["delta"]
-    )
-    evaluator.eval(data_loaders[-1], gpu=gpu_id)
+    evaluator = Evaluator(model=net, ckpt_file=trainer.out_dir / trainer.best["ckpt"], out_dir=out_dir, delta=delta)
+    evaluator.eval(data_loaders[-1], gpu=gpus[0])
 
 
 @cli.command(help="Evaluate.")
@@ -105,36 +118,50 @@ def train(**args):
 @click.option("--te/--no-te", help="With/without time embedding.", default=True)
 @click.option("--out-timesteps", type=int, help="Number of output timesteps.", default=12)
 @click.option("--timesteps-per-hour", type=int, help="Timesteps per hour.", default=12)
-def eval(**args):
+def eval(
+    adj_file,
+    data_file,
+    ckpt_file,
+    out_dir,
+    num_nodes,
+    num_channels,
+    num_workers,
+    in_hours,
+    batch_size,
+    model,
+    gpus,
+    delta,
+    te,
+    out_timesteps,
+    timesteps_per_hour,
+):
+    in_hours, gpus = [int(i) for i in in_hours.split(",")], [int(i) for i in gpus.split(",")]
     # data loaders
     data_loaders = load_data(
-        data_file=args["data_file"],
-        batch_size=args["batch_size"],
-        in_hours=[int(h) for h in args["in_hours"].split(",")],
-        out_timesteps=args["out_timesteps"],
-        timesteps_per_hour=args["timesteps_per_hour"],
-        num_workers=args["num_workers"],
+        data_file=data_file,
+        batch_size=batch_size,
+        in_hours=in_hours,
+        out_timesteps=out_timesteps,
+        timesteps_per_hour=timesteps_per_hour,
+        num_workers=num_workers,
     )
     # network
-    net = models[args["model"]](
-        n_components=len(args["in_hours"].split(",")),
-        in_channels=args["num_channels"],
-        in_timesteps=args["timesteps_per_hour"],
-        out_timesteps=args["out_timesteps"],
-        adjacency=load_adj(args["adj_file"], args["num_nodes"]),
-        use_te=args["te"],
+    net = models[model](
+        n_components=len(in_hours),
+        in_channels=num_channels,
+        in_timesteps=timesteps_per_hour,
+        out_timesteps=out_timesteps,
+        adjacency=load_adj(adj_file, num_nodes),
+        use_te=te,
         init_params=False,
     )
     # enable GPU.
-    if len(args["gpus"].split(",")) > 1:
-        gpu_id = None
-        net = DataParallel(net, device_ids=[int(i) for i in args["gpus"].split(",")]).cuda()
-    else:
-        gpu_id = int(args["gpus"])
-        net = net.cuda(gpu_id)
+    if len(gpus) > 1:
+        net = DataParallel(net, device_ids=gpus)
+    net = net.cuda(gpus[0])
     # eval
-    evaluator = Evaluator(model=net, ckpt_file=args["ckpt_file"], out_dir=args["out_dir"], delta=args["delta"])
-    evaluator.eval(data_loaders[-1], gpu=gpu_id)
+    evaluator = Evaluator(model=net, ckpt_file=ckpt_file, out_dir=out_dir, delta=delta)
+    evaluator.eval(data_loaders[-1], gpu=gpus[0])
 
 
 if __name__ == "__main__":
