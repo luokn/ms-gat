a826326311682960ddd4f3a5a7777f4299b69420
Luokun <olooook@outlook.com>
2021-12-16 10:43:55 +0800

    .

--- train.py
+++ train.py
@@ -8,6 +8,7 @@
 
 
 import click
+import yaml
 from torch.nn import DataParallel
 
 from data import load_adj, load_data
@@ -18,59 +19,54 @@ models = {"ms-gat": msgat.msgat72, "ms-gat48": msgat.msgat48, "ms-gat72": msgat.
 
 
 @click.command(help="Train.")
-@click.option("-a", "--adj-file", type=str, help="Graph adjacency file.")
-@click.option("-d", "--data-file", type=str, help="Time series data file.")
-@click.option("-f", "--ckpt-file", type=str, help="Pre-saved checkpoint file.")
-@click.option("-o", "--out-dir", type=str, help="Output directory for logs and checkpoints.")
-@click.option("-n", "--num-nodes", type=int, help="Number of nodes in the graph.")
-@click.option("-c", "--num-channels", type=int, help="Number of time series data channels.")
-@click.option("-j", "--num-workers", type=int, help="Number of data loader workers.", default=0)
-@click.option("-i", "--in-hours", type=str, help="Sampling hours.", default="1,2,3,24,168")
+@click.argument("dataset", type=str)
+@click.option("-c", "--ckpt", type=str, help="Checkpoint file.")
+@click.option("-i", "--in-hours", type=str, help="Input hours.", default="1,2,3,24,168")
+@click.option("-o", "--out-dir", type=str, help="Output directory.")
+@click.option("-j", "--num-workers", type=int, help="Number of DataLoader workers.", default=0)
 @click.option("-b", "--batch-size", type=int, help="Batch size.", default=64)
+@click.option("--model", type=str, help="Model name.", default="ms-gat")
+@click.option("--delta", type=float, help="HuberLoss delta.", default=50)
+@click.option("--gpus", type=str, help="GPUs.", default="0")
 @click.option("--min-epochs", type=int, help="Min epochs.", default=10)
 @click.option("--max-epochs", type=int, help="Max epochs.", default=100)
-@click.option("--model", type=click.Choice(models.keys()), help="Model.", default="ms-gat")
-@click.option("--gpus", type=str, help="GPUs.", default="0")
-@click.option("--delta", type=float, help="Huber loss delta.", default=50)
-@click.option("--te/--no-te", help="With/without time embedding.", default=True)
 @click.option("--out-timesteps", type=int, help="Number of output timesteps.", default=12)
-@click.option("--timesteps-per-hour", type=int, help="Timesteps per hour.", default=12)
+@click.option("--te/--no-te", help="With/without TE.", default=True)
 def train(
-    adj_file,
-    data_file,
-    ckpt_file,
-    out_dir,
-    num_nodes,
-    num_channels,
-    num_workers,
+    dataset,
+    *,
+    ckpt,
     in_hours,
+    out_dir,
+    num_workers,
     batch_size,
+    model,
+    delta,
+    gpus,
     min_epochs,
     max_epochs,
-    model,
-    gpus,
-    delta,
-    te,
     out_timesteps,
-    timesteps_per_hour,
+    te,
 ):
     in_hours, gpus = [int(i) for i in in_hours.split(",")], [int(i) for i in gpus.split(",")]
+    with open("data.yaml", "r") as f:
+        dataset = yaml.load(f, Loader=yaml.CLoader)
     # data loaders
     data_loaders = load_data(
-        data_file=data_file,
+        data_file=dataset["data-file"],
         batch_size=batch_size,
         in_hours=in_hours,
         out_timesteps=out_timesteps,
-        timesteps_per_hour=timesteps_per_hour,
+        timesteps_per_hour=dataset["timesteps-per-hour"],
         num_workers=num_workers,
     )
     # model
     model = models[model](
         n_components=len(in_hours),
-        in_channels=num_channels,
-        in_timesteps=timesteps_per_hour,
+        in_channels=dataset["num_channels"],
+        in_timesteps=dataset["timesteps-per-hour"],
         out_timesteps=out_timesteps,
-        adjacency=load_adj(adj_file, num_nodes),
+        adjacency=load_adj(dataset["adj-file"], dataset["num-nodes"]),
         use_te=te,
     )
     # enable GPU.
@@ -91,8 +87,8 @@ def train(
         min_epochs=min_epochs,
         min_delta=1e-4,
     )
-    if ckpt_file:
-        trainer.load(ckpt_file=ckpt_file)
+    if ckpt:
+        trainer.load(ckpt_file=ckpt)
     trainer.fit((data_loaders[0], data_loaders[1]), gpu=gpus[0])
     print("Training completed!")
     # eval
